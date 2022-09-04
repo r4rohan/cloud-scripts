2e0dffa34d9216a1650c2e8e35be5c0d90b0cc4e
Rohan Singh <rohan.singh@sada.com>
2022-09-04 15:31:12 +0400

    add cloudbuild folder

---
+++ scripts/cloudbuild/gce-tf/main.tf
@@ -0,0 +1,32 @@
+resource "google_service_account" "loki" {
+  account_id   = "loki"
+  display_name = "Loki Service Account"
+}
+
+resource "google_compute_instance" "loki" {
+  name         = "loki"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["temp"]
+
+  boot_disk {
+    initialize_params {
+      size  = 30
+      type  = "pd-standard"
+      image = ubuntu-1804-lts
+    }
+  }
+
+  network_interface {
+    network = "thanos"
+    access_config {
+      network_tier = "PREMIUM"
+    }
+  }
+
+  service_account {
+    email  = google_service_account.loki.email
+    scopes = ["cloud-platform"]
+  }
+}
