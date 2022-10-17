691931a4a0e74ad2ffde59cb831126df147cb529
Rohan Singh <rohan.singh@sada.com>
2022-10-17 23:51:07 +0400

    update artifact download steps

--- scripts/cloudbuild/gce-tf/main.tf
+++ scripts/cloudbuild/gce-tf/main.tf
@@ -1,9 +1,11 @@
 resource "google_service_account" "loki" {
+  project      = "rohan-orbit"
   account_id   = "god-loki"
   display_name = "Loki Service Account"
 }
 
 resource "google_compute_instance" "loki" {
+  project      = "rohan-orbit"
   name         = "loki"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
