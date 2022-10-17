2fe4aab1cfdd11d371eab00d91cdae6049f620c4
Rohan Singh <rohan.singh@sada.com>
2022-10-17 20:11:54 +0400

    tf fmt

--- scripts/cloudbuild/gce-tf/main.tf
+++ scripts/cloudbuild/gce-tf/main.tf
@@ -8,7 +8,7 @@ resource "google_compute_instance" "loki" {
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
-  tags = ["temp"]
+  tags = ["temporary"]
 
   boot_disk {
     initialize_params {
